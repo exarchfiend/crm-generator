fa9330a1e4ce7d5d899aabf9a6b705a10099d396
mjauto <2673145040@qq.com>
2023-09-11 19:14:52 -0700

    第一次提交，基础模板

---
+++ src/main/java/fun/mjauto/crm/CrmGeneratorApplication.java
@@ -0,0 +1,13 @@
+package fun.mjauto.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fun/mjauto/crm/CrmGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.mjauto.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
